2b1b514c188cfd424396a7e6e686d58178c9e64d
remberf1 <68003095+remberf1@users.noreply.github.com>
2026-01-30 17:24:10 +0100

    admin and onwer post works

--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -9,7 +9,7 @@ router.post('/create',protect, createPost);           // POST /api/v1/posts
 router.get('/', getPosts);             // GET /api/v1/posts
 router.get('/:id', getPostById);       // GET /api/v1/posts/:id
 router.patch('/:id',protect, updatePost);      // PATCH /api/v1/posts/:id
-router.delete('/delete',protect, deletePost);
+router.delete('/delete',isAdmin, deletePost);
 router.delete('/delete/:id',protect, deletePostById);
 
 export default router;
